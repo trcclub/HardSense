5ebd1303a5eef2a108ce9d9bd5065343232b662d
Kitecraft Unborn <kitecraft@gmail.com>
2019-10-07 07:09:15 -0600

    Adding initial ESP32 fireware

---
+++ Software/ESP32/HardSenseESP/HardSenseESP.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		HardSenseESP.ino
+ Created:	10/7/2019 7:07:38 AM
+ Author:	Terry
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
