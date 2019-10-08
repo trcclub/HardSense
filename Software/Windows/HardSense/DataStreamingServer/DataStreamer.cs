6267fd718ffed7a8bcad323b44b1d33881a6fdeb
Kitecraft Unborn <kitecraft@gmail.com>
2019-10-08 07:56:33 -0600

    New DataStreamer class with threading.  This now handles the read and writing to the socket and will also be where the memory mapped file is polled.

---
+++ Software/Windows/HardSense/DataStreamingServer/DataStreamer.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace HardSense.DataStreamingServer
+{
+
+    public class StateObject
+    {
+        // Client  socket.  
+        public Socket workSocket = null;
+        // Size of receive buffer.  
+        public const int BufferSize = 1024;
+        // Receive buffer.  
+        public byte[] buffer = new byte[BufferSize];
+        // Received data string.  
+        public StringBuilder sb = new StringBuilder();
+    }
+
+    public class DataStreamer
+    {
+        private Thread readThread;
+        private Thread writeThread;
+
+        private bool running = false;
+
+        private Socket socketToStreamTo;
+        private StateObject state = new StateObject();
+
+        public DataStreamer(Socket socket)
+        {
+            socketToStreamTo = socket;
+            readThread = new Thread(readThreadProc);
+            writeThread = new Thread(writeThreadProc);
+        }
+
+        
+        public void ThreadProc()
+        {
+            readThread.Start();
+            writeThread.Start();
+            while(DataStreamingServer.continueRunning)
+            {
+                Thread.Sleep(50);
+            }
+
+            Stop();
+        }
+
+
+        private void Stop()
+        {
+            if(!running)
+            {
+                return;
+            }
+            readThread.Join();
+            writeThread.Join();
+            running = false;
+        }
+        private void readThreadProc()
+        {
+            while(DataStreamingServer.continueRunning)
+            {
+                int bytesRead = socketToStreamTo.Receive(state.buffer, 0, StateObject.BufferSize, 0);
+                if (bytesRead > 0)
+                {
+                    state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+                    string leftOverContent = HandleInput(state.sb.ToString());
+                    state.sb.Clear();
+                    state.sb.Append(leftOverContent);
+                }
+                Thread.Sleep(20);
+            }
+
+        }
+        private string HandleInput(string inputData)
+        {
+            string ret = "";
+            if (inputData.Length == 0)
+            {
+                return ret;
+            }
+
+            int firstInstanceOfStart = inputData.IndexOf(ProtocolKeys.TRANSMISSION_KEYS["TRANS_START"]);
+            if (firstInstanceOfStart > 0)
+            {
+                inputData = inputData.Substring(firstInstanceOfStart);
+            }
+
+            if (!inputData.StartsWith(ProtocolKeys.TRANSMISSION_KEYS["TRANS_START"]))
+            {
+                if (inputData.IndexOf(ProtocolKeys.TRANSMISSION_KEYS["TRANS_START"]) > 0)
+                {
+                    inputData = inputData.Substring(inputData.IndexOf(ProtocolKeys.TRANSMISSION_KEYS["TRANS_START"]));
+                }
+                else
+                {
+                    return ret;
+                }
+            }
+
+
+            int endMarkerLocation = inputData.IndexOf(ProtocolKeys.TRANSMISSION_KEYS["TRANS__ETX"]);
+            if (endMarkerLocation != inputData.Length)
+            {
+                if (endMarkerLocation > 0)
+                {
+                    string tmpContent = inputData.Substring(0, inputData.IndexOf(ProtocolKeys.TRANSMISSION_KEYS["TRANS__ETX"]) + 1);
+                    ret = inputData.Substring(inputData.IndexOf(ProtocolKeys.TRANSMISSION_KEYS["TRANS__ETX"]) + 1);
+                    parseInput(tmpContent);
+                    return HandleInput(ret);
+                }
+                else
+                {
+                    return inputData;
+                }
+            }
+
+
+            parseInput(inputData);
+            return ret;
+        }
+
+        private void parseInput(string inputData)
+        {
+            string strippedData = inputData.Substring(1, inputData.Length - 2);
+            strippedData += ProtocolKeys.TRANSMISSION_KEYS["TRANS__ETX"];
+            Send(strippedData);
+        }
+
+        private void writeThreadProc()
+        {
+            while (DataStreamingServer.continueRunning)
+            {
+                Thread.Sleep(500);
+            }
+
+        }
+
+        private void Send(String data)
+        {
+            // Convert the string data to byte data using ASCII encoding.  
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+            socketToStreamTo.Send(byteData, 0, byteData.Length, 0);
+        }
+    }
+}
---
+++ Software/Windows/HardSense/DataStreamingServer/ProtocolKeys.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardSense.DataStreamingServer
+{
+    class ProtocolKeys
+    {
+        public static Dictionary<string, string> TRANSMISSION_KEYS = new Dictionary<string, string>();
+        public static void init()
+        {
+            TRANSMISSION_KEYS.Add("TRANS_START", "\u0002");
+            TRANSMISSION_KEYS.Add("TRANS__ETX", "\u0003");
+            TRANSMISSION_KEYS.Add("TRANS__DATA_SIZE_END", "\u001F");
+            TRANSMISSION_KEYS.Add("TRANS__PACKET_SIZE_END", "\u001E");
+            TRANSMISSION_KEYS.Add("TRANS__PACKET_END", "\u001D");
+            TRANSMISSION_KEYS.Add("TRANS__ACK", "\u0006");
+            TRANSMISSION_KEYS.Add("TRANS__NACK", "\u0015");
+
+            TRANSMISSION_KEYS.Add("TRANS__CONNECTION_REQUEST", "\u0005");
+            TRANSMISSION_KEYS.Add("TRANS__CONNECTION_ACK", "\u0001");
+            TRANSMISSION_KEYS.Add("TRANS__DISCONNECT", "\u0004");
+
+            TRANSMISSION_KEYS.Add("TRANS__REQUESTING_FULL_SENSOR_LIST", "\u0021");
+            TRANSMISSION_KEYS.Add("TRANS__SENDING_FULL_SENSOR_LIST", "\u0022");
+            TRANSMISSION_KEYS.Add("TRANS__REQUEST_SENSORS_FOR_STREAMING", "\u0023");
+            TRANSMISSION_KEYS.Add("TRANS__UPDATE_SENSOR_VALUE", "\u0024");
+
+        }
+    }
+}
