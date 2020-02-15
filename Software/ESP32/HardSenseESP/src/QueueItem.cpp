8b2fd8389e4256bfbdd1923887ccec771cdbf6b6
Kitecraft Unborn <kitecraft@gmail.com>
2020-02-15 14:03:47 -0700

    Added the new cpp file.  This was missed earlier.  oops.

---
+++ Software/ESP32/HardSenseESP/src/QueueItem.cpp
@@ -0,0 +1,36 @@
+#include "QueueItem.h"
+
+
+Queues::Queues()
+{
+	DataQueue<QUEUE_ITEM> displayQueue(20);
+	DataQueue<QUEUE_ITEM> outputQueue(10);
+
+    displayQueueMux = portMUX_INITIALIZER_UNLOCKED;
+    outputQueueMux = portMUX_INITIALIZER_UNLOCKED;
+}
+
+Queues::~Queues()
+{
+
+}
+
+void Queues::AddItemToDisplayQueue(char key, String value)
+{
+	QUEUE_ITEM qi;
+	qi.key = key;
+	qi.value = value;
+	portENTER_CRITICAL(&displayQueueMux);
+	displayQueue.enqueue(qi);
+	portEXIT_CRITICAL(&displayQueueMux);
+}
+
+void Queues::AddItemToOutputQueue(char key, String value)
+{
+	QUEUE_ITEM qi;
+	qi.key = key;
+	qi.value = value;
+	portENTER_CRITICAL(&outputQueueMux);
+	outputQueue.enqueue(qi);
+	portEXIT_CRITICAL(&outputQueueMux);
+}
